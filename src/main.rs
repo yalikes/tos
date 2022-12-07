85cbe32a7badf6f990419faf3f9ff429fcfaddd5
paohui <2054453988@qq.com>
2022-12-07 14:53:25 +0800

    add support with print! and println! macro

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
+
 mod memolayout;
+mod uart;
 
 use core::{arch::global_asm, panic::PanicInfo};
-use uart_16550::MmioSerialPort;
+
 
 
 global_asm!(include_str!("entry.asm"));
@@ -13,16 +15,8 @@ static STACK0: [u8; 4096] = [0; 4096];
 
 #[no_mangle]
 pub extern "C" fn start() -> !{
-    let mut serial_port = unsafe { MmioSerialPort::new(memolayout::UART)};
-    serial_port.init();
-
-    for i in 0..128{
-        serial_port.send(i);
-    }
+    println!("Hello World!");
     loop {
-        let x = serial_port.receive();
-        serial_port.send(x);
-        serial_port.send(x);
     }
 
 }
---
+++ src/uart.rs
@@ -0,0 +1,29 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use uart_16550::MmioSerialPort;
+use spin::Mutex;
+use crate::memolayout;
+
+lazy_static!{
+    pub static ref terminal_writer: Mutex<MmioSerialPort> = Mutex::new(
+        unsafe { MmioSerialPort::new(memolayout::UART)
+    });
+}
+
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::uart::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments){
+    use core::fmt::Write;
+    terminal_writer.lock().write_fmt(args).unwrap();
+}
