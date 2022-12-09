ecf5f85ba2c43b6e24ca6488179e2bb615220011
paohui <2054453988@qq.com>
2022-12-09 16:55:46 +0800

    cargo run use run.sh as runner

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #rust-objcopy --strip-all target/riscv64gc-unknown-none-elf/release/tos \
 #        -O binary target/riscv64gc-unknown-none-elf/release/tos.bin
+#qemu-system-riscv64 \
+#            -machine virt \
+#            -nographic \
+#            -m 128M \
+#            -bios none \
+#            -kernel target/riscv64gc-unknown-none-elf/debug/tos \
+#            -S -gdb tcp::4321
+
 qemu-system-riscv64 \
             -machine virt \
             -nographic \
             -m 128M \
             -bios none \
-            -kernel target/riscv64gc-unknown-none-elf/debug/tos \
-            -S -gdb tcp::4321
+            -kernel target/riscv64gc-unknown-none-elf/debug/tos
