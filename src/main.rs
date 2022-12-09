519d00817d9292ad047ae76000647c7f8aab71e7
paohui <2054453988@qq.com>
2022-12-10 00:43:06 +0800

    delete recursion test function foo

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,11 @@ static STACK0: [u8; 4096] = [0; 4096];
 #[no_mangle]
 pub extern "C" fn start() -> !{
     println!("Hello World!");
-    foo(120);
     loop {
     }
 
 }
 
-fn foo(i: usize){
-    if i <= 0{
-        return
-    }
-    let x = 12;
-    foo(i-1);
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
