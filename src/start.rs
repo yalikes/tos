bda8d7d00b652b61cf7b593201d65d7b455163ce
paohui <2054453988@qq.com>
2022-12-13 17:09:37 +0800

    finish r_mstatus function

--- src/start.rs
+++ src/start.rs
@@ -1,16 +1,24 @@
-use crate::{println, main};
+use crate::{main, println};
+use core::arch::asm;
 
 #[no_mangle]
 static TIMER_SCRATCH: [u8; 5] = [0; 5];
 
 #[no_mangle]
-extern "C" fn start(){
+extern "C" fn start() {
+    // set M Previous Privilege mode to Supervisor, for mret.
     println!("starting");
     let x: u64 = r_mstatus();
     main();
 }
 
-fn r_mstatus() -> u64{
-    let x = 42;
+fn r_mstatus() -> u64 {
+    let mut x: u64;
+    unsafe {
+        asm! {
+            "csrr {x}, mstatus",
+            x = out(reg) x
+        } //volatile by default
+    }
     x
 }
