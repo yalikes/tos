2aa4e99a78fc8a228afb23eacc3cf05f3cf24f78
paohui <2054453988@qq.com>
2022-12-14 08:37:43 +0800

    add alloc bintin feature

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod start;
 mod riscv;
 
 use core::{arch::global_asm, panic::PanicInfo};
+use linked_list_allocator::LockedHeap;
+use alloc::{vec, vec::Vec};
 
-
+extern crate alloc;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("trampoline.asm"));
@@ -16,9 +18,19 @@ global_asm!(include_str!("trampoline.asm"));
 #[no_mangle]
 static STACK0: [u8; 4096] = [0; 4096];
 
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
 #[no_mangle]
 pub extern "C" fn main() -> !{
-    println!("Hello World!");
+    let heap_start = crate::memolayout::get_kernel_end();
+    let heap_end = crate::memolayout::PHYSTOP;
+    let heap_size = heap_end - heap_start;
+    unsafe{
+        ALLOCATOR.lock().init(heap_start as *mut u8, heap_size);
+    }
+    let mut v = Vec::new();
+    v.push(12);
     loop {
     }
 
--- src/memolayout.rs
+++ src/memolayout.rs
@@ -1,1 +1,13 @@
 pub const UART: usize = 0x1000_0000;
+pub const KERNELBASE: usize = 0x8000_0000;
+pub const PHYSTOP: usize = KERNELBASE + 128 * 1024 * 1024;
+
+extern "C" {
+    pub static end: u8;
+}
+
+pub fn get_kernel_end() -> usize{
+    unsafe {
+        return (&end as *const u8) as usize;
+    }
+}
