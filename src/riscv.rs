70ab3decb803f9d19dd4eac3b86085cd3f42a380
paohui <2054453988@qq.com>
2022-12-14 16:20:53 +0800

    temporary save part of riscv specific helper functions

--- src/riscv.rs
+++ src/riscv.rs
@@ -1,10 +1,10 @@
 use core::arch::asm;
 
-pub const MSTATUS_MPP_MASK:u64 = 3 << 11;
-pub const MSTATUS_MPP_M:u64 = 3 << 11;
-pub const MSTATUS_MPP_S:u64 = 1 << 11;
-pub const MSTATUS_MPP_U:u64 = 0 << 11;
-pub const MSTATUS_MIE:u64 = 1 << 3;
+pub const MSTATUS_MPP_MASK: u64 = 3 << 11;
+pub const MSTATUS_MPP_M: u64 = 3 << 11;
+pub const MSTATUS_MPP_S: u64 = 1 << 11;
+pub const MSTATUS_MPP_U: u64 = 0 << 11;
+pub const MSTATUS_MIE: u64 = 1 << 3;
 
 #[inline]
 pub fn r_mhartid() -> u64 {
@@ -35,6 +35,149 @@ pub fn w_mstatus(x: u64) {
     unsafe {
         asm! {
             "csrw mstatus, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+#[inline]
+pub fn w_mepc(x: u64){
+    unsafe {
+        asm! {
+            "csrw mepc, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+// Supervisor Status Register, sstatus
+pub const SSTATUS_SSP: u64 = 1<<8;
+pub const SSTATUS_SPIE: u64 = 1<<5;
+pub const SSTATUS_UPIE: u64 = 1<<4;
+pub const SSTATUS_SIE: u64 = 1<<1;
+pub const SSTATUS_UIE: u64 = 1<<0;
+
+
+
+#[inline]
+pub fn r_sstatus() -> u64{
+    let mut x;
+    unsafe {
+        asm! {
+            "csrr {x}, sstatus",
+            x = out(reg) x
+        } //volatile by default
+    }
+    x
+}
+
+#[inline]
+pub fn w_sstatus(x: u64){
+    unsafe {
+        asm! {
+            "csrw sstatus, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+// Supervisor Interrupt Pending
+#[inline]
+pub fn r_sip() -> u64{
+    let mut x;
+    unsafe {
+        asm! {
+            "csrr {x}, sip",
+            x = out(reg) x
+        } //volatile by default
+    }
+    x
+}
+
+#[inline]
+pub fn w_sip(x: u64){
+    unsafe {
+        asm! {
+            "csrw sip, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+//Supervisor Interupt Enable
+pub const SIE_SEIE: u64 = 1<<9;//external
+pub const SIE_STIE: u64 = 1<<5;//timer
+pub const SIE_SSIE: u64 = 1<<1;//software
+
+#[inline]
+pub fn r_sie() -> u64{
+    let mut x;
+    unsafe {
+        asm! {
+            "csrr {x}, sie",
+            x = out(reg) x
+        } //volatile by default
+    }
+    x
+}
+
+#[inline]
+pub fn w_sie(x: u64){
+    unsafe {
+        asm! {
+            "csrw sie, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+// Machine-mode Interrupt Enable
+pub const MIE_SEIE: u64 = 1<<9;//external
+pub const MIE_STIE: u64 = 1<<5;//timer
+pub const MIE_SSIE: u64 = 1<<1;//software
+
+#[inline]
+pub fn r_mie() -> u64{
+    let mut x;
+    unsafe {
+        asm! {
+            "csrr {x}, mie",
+            x = out(reg) x
+        } //volatile by default
+    }
+    x
+}
+
+#[inline]
+pub fn w_mie(x: u64){
+    unsafe {
+        asm! {
+            "csrw mie, {x}",
+            x = in(reg) x
+        } //volatile by default
+    }
+}
+
+// supervisor exception program counter, holds the
+// instruction address to which a return from
+// exception will go.
+#[inline]
+pub fn r_sepc() -> u64{
+    let mut x;
+    unsafe {
+        asm! {
+            "csrr {x}, sepc",
+            x = out(reg) x
+        } //volatile by default
+    }
+    x
+}
+
+#[inline]
+pub fn w_sepc(x: u64){
+    unsafe {
+        asm! {
+            "csrw sepc, {x}",
             x = in(reg) x
         } //volatile by default
     }
--- src/start.rs
+++ src/start.rs
@@ -1,5 +1,5 @@
 use crate::{main, println};
-use crate::riscv::{r_mstatus, w_mstatus, };
+use crate::riscv::{r_mstatus, w_mstatus, w_mepc, MSTATUS_MPP_MASK, MSTATUS_MPP_S};
 
 #[no_mangle]
 static TIMER_SCRATCH: [u8; 5] = [0; 5];
@@ -8,8 +8,13 @@ static TIMER_SCRATCH: [u8; 5] = [0; 5];
 extern "C" fn start() {
     // set M Previous Privilege mode to Supervisor, for mret.
     println!("starting");
-    let x: u64 = r_mstatus();
-    main();
+    let mut x: u64 = r_mstatus();
+    x &= !MSTATUS_MPP_MASK;
+    x |= MSTATUS_MPP_S;
+    w_mstatus(x);
+
+    //set M Exception Program Counter to main, for mret
+    w_mepc(main as u64);
 }
 
 
