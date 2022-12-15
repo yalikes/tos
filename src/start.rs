770e5c38a4943ea7178f1323e3cc2d020c1e7fd8
paohui <2054453988@qq.com>
2022-12-15 18:51:32 +0800

    add part of start function

--- src/start.rs
+++ src/start.rs
@@ -1,5 +1,7 @@
+use core::arch::asm;
+
 use crate::{main, println};
-use crate::riscv::{r_mstatus, w_mstatus, w_mepc, MSTATUS_MPP_MASK, MSTATUS_MPP_S};
+use crate::riscv::*;
 
 #[no_mangle]
 static TIMER_SCRATCH: [u8; 5] = [0; 5];
@@ -15,7 +17,26 @@ extern "C" fn start() {
 
     //set M Exception Program Counter to main, for mret
     w_mepc(main as u64);
+    w_satp(0);
+    // delegate all interrupts and exceptions to supervisor mode.
+    w_medeleg(0xffff);
+    w_mideleg(0xffff);
+    w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
+    // configure Physical Memory Protection to give supervisor mode
+    // access to all of physical memory.
+    w_pmpaddr0(0x3fffffffffffff);
+    w_pmpcfg0(0xf);
+    timerinit();
+    let id = r_mhartid();
+    w_tp(id);
+    unsafe{asm!("mret");}
 }
 
 
+fn timerinit(){
+    let id = r_mhartid();
+    let interval = 1000000; // cycles; about 1/10th second in qemu.
+
+
+}
 
