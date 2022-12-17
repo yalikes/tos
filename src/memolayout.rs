1ad333d8165036b39a709abf393fd923b1ed62c4
paohui <2054453988@qq.com>
2022-12-17 23:48:34 +0800

    add CLINT_MTIMECMP and CLINT_MTIME

--- src/memolayout.rs
+++ src/memolayout.rs
@@ -1,6 +1,8 @@
 pub const UART: usize = 0x1000_0000;
 pub const KERNELBASE: usize = 0x8000_0000;
 pub const PHYSTOP: usize = KERNELBASE + 128 * 1024 * 1024;
+pub const CLINT: usize = 0x200_0000;
+pub const CLINT_MTIME: usize = CLINT + 0xBFF8;
 
 extern "C" {
     pub static end: u8;
@@ -11,3 +13,9 @@ pub fn get_kernel_end() -> usize{
         return (&end as *const u8) as usize;
     }
 }
+
+#[inline]
+pub fn CLINT_MTIMECMP(hartid: u64) -> u64{
+    return (CLINT as u64) + 0x4000 + 8*hartid;
+}
+
