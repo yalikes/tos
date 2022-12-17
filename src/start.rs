1b2adfdefd73315091d68a2dd857eb172658f649
paohui <2054453988@qq.com>
2022-12-17 23:49:25 +0800

    finish start.rs

--- src/start.rs
+++ src/start.rs
@@ -2,9 +2,14 @@ use core::arch::asm;
 
 use crate::{main, println};
 use crate::riscv::*;
+use crate::memolayout::{CLINT_MTIMECMP, CLINT_MTIME};
 
 #[no_mangle]
-static TIMER_SCRATCH: [u8; 5] = [0; 5];
+static mut TIMER_SCRATCH: [u64; 5] = [0; 5];
+
+extern "C" {
+    fn timervec();
+}
 
 #[no_mangle]
 extern "C" fn start() {
@@ -36,7 +41,28 @@ extern "C" fn start() {
 fn timerinit(){
     let id = r_mhartid();
     let interval = 1000000; // cycles; about 1/10th second in qemu.
+    let timer_addr: *mut u64 = CLINT_MTIMECMP(id) as *mut u64;
+    let mtime_addr: *mut u64 = CLINT_MTIME as *mut u64;
+    unsafe {
+        *timer_addr = *mtime_addr + interval;
+    }
+    // prepare information in scratch[] for timervec.
+    // scratch[0..2] : space for timervec to save registers.
+    // scratch[3] : address of CLINT MTIMECMP register.
+    // scratch[4] : desired interval (in cycles) between timer interrupts.
+    unsafe{
+        TIMER_SCRATCH[3] = CLINT_MTIMECMP(id);
+        TIMER_SCRATCH[4] = interval;
+        w_mscratch((&TIMER_SCRATCH as *const u64) as u64);
+    }
+    // set the machine-mode trap handler
+    w_mtvec(timervec as u64);
+    
+    // enable machine-mode interrupts
+    w_mstatus(r_mstatus() | MSTATUS_MIE);
 
-
+    //enable machine-mode timer interrupts
+    w_mie(r_mie() | MIE_MTIE);
+    println!("{}: itimer inited", id);
 }
 
