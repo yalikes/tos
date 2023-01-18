ce1891e9e4695437aa112af2e5d4c4bd8be8acb7
paohui <2054453988@qq.com>
2023-01-18 23:44:04 +0800

    partiallt implement usertrap function

--- src/trap.rs
+++ src/trap.rs
@@ -1,9 +1,11 @@
+use core::panic;
+
 use crate::MAKE_SATP;
 use crate::memolayout::{get_kernelvec, get_trampoline, get_uservec, get_userret,TRAMPOLINE, TRAPFRAME};
-use crate::proc::{proc, procid};
+use crate::proc::{proc, procid, Trapframe};
 use crate::riscv::{
     r_satp, r_sstatus, r_tp, w_sepc, w_sstatus, w_stvec,
-    PGSIZE, SSTATUS_SPIE, SSTATUS_SPP, SATP_SV39
+    PGSIZE, SSTATUS_SPIE, SSTATUS_SPP, SATP_SV39, r_sepc, r_scause
 };
 
 // set up to take exceptions and traps while in the kernel.
@@ -25,7 +27,7 @@ pub fn usertrapret() {
         let trapframe = &mut (*p.trapframe);
         trapframe.kernel_satp = r_satp(); // kernel page table
         trapframe.kernel_sp = p.kstack + PGSIZE as u64; // process's kernel stack
-                                                        // trapframe.kernel_trap = usertrap;
+        trapframe.kernel_trap = usertrap as u64;
         trapframe.kernel_hartid = r_tp(); // hartid for cpuid()
     }
     // set up the registers that trampoline.asm's sret will use
@@ -40,11 +42,38 @@ pub fn usertrapret() {
     let epc = unsafe { (*p.trapframe).epc };
     w_sepc(epc);
     let satp = MAKE_SATP!(unsafe{(&(*p.pagetable).ptes as *const [u64; 512]) as u64});
+    drop(p);
     let fn_ptr_addr = (TRAMPOLINE + (get_userret() - get_trampoline())) as *const ();
     let fn_ptr: extern "C" fn(u64, u64) -> () = unsafe{ core::mem::transmute(fn_ptr_addr)};
     fn_ptr(TRAPFRAME as u64, satp);
 }
 
 pub fn usertrap() {
-    if (r_sstatus() & SSTATUS_SPP) != 0 {}
+    // when a exception occurs, before we disable exception, another excpetion occurs
+    // does this program can handle this?
+    if (r_sstatus() & SSTATUS_SPP) != 0{
+        panic!("usertrap: not from user mode");
+    }
+    // send interrupts and exceptions to kerneltrap(),
+    // since we're now in the kernel.
+    w_stvec(get_kernelvec() as u64);
+    let proc_index = procid().unwrap();
+    unsafe{
+        let mut proc_guard = proc[proc_index].write();
+        let trapfram: &mut Trapframe = &mut (*proc_guard.trapframe);
+        trapfram.epc = r_sepc();
+        if r_scause() == 8{
+            //syscall
+            if proc_guard.killed {
+                //process killed
+                loop {}
+            }
+            // sepc points to the ecall instruction,
+            // but we want to return to the next instruction.
+            trapfram.epc += 4;
+            
+            // an interrupt will change sstatus &c registers,
+            // so don't enable until done with those registers.
+        }
+    }
 }
